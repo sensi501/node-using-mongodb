23b46caae9c03679c2914400843173a4a6a42ffa
Jamal Pilgrim <sensi501@gmail.com>
2020-12-26 03:03:25 -0500

    Completed module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/productsdb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
 const app = express();
 const PORT = 4000;
 
